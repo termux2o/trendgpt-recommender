f736f52550b636a8ce50a3715199469cc26d96c0
aakash_sharma <ak2855001@gmail.com>
2025-09-23 12:32:04 +0530

    connection script

--- backend/my_utils/main_connection_test.py
+++ backend/my_utils/main_connection_test.py
@@ -1,9 +1,11 @@
 # main.py
-from my_utils.mongo_db_class import MongoDBClient
-
+from mongo_db_class import MongoDBClient # because it is in same folder 
+# if parent folder is diffrenet then 'my_utils.mongo_db_class'"
 mongo_client = MongoDBClient()
 db = mongo_client.connect()
 print("Collections:", mongo_client.get_collections())
-
 appliances_collection = mongo_client.get_collection("Appliances")
+appliances_reviews = mongo_client.get_collection("Appliances_reviews")
 print("Sample document:", appliances_collection.find_one())
+print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+print("sample_review:",appliances_reviews.find_one())
